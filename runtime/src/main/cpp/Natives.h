be9e90e5694712d9c8d4eebd4806ee98b8bb894d
Pavel Punegov <pavel.punegov@jetbrains.com>
2018-07-03 12:09:30 +0300

    Remove old declarations

--- runtime/src/main/cpp/Natives.h
+++ runtime/src/main/cpp/Natives.h
@@ -136,10 +136,6 @@ OBJ_GETTER0(Kotlin_konan_internal_undefined);
 void Kotlin_konan_internal_GC_suspend(KRef);
 void Kotlin_konan_internal_GC_resume(KRef);
 
-// Random operations.
-KInt Kotlin_random();
-void Kotlin_srandom(KInt seed);
-
 #ifdef __cplusplus
 }
 #endif
